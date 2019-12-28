301d7533034066187611dc06d91150f812d41b6e
Faisal Ahmed <faisal95bd@gmail.com>
2019-12-29 00:37:13 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>Faisal's Cars is an organized company that rents cars and other vehicles to clients at lower costs. We we are here to serve every Bangladeshi Citizen</p>
+					<ul>
+						<li><a href="https://www.facebook.com/faisal95sourav" class="facebook" target="_blank"></a></li>
+						<li><a href="https://www.facebook.com/faisal95sourav" class="twitter" target="_blank"></a></li>
+						<li><a href="https://www.facebook.com/faisal95sourav" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "https://www.facebook.com/faisal95sourav">Faial Ahmed</a>
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
